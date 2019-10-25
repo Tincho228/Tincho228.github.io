d1116a91d9ff184a660c24bb2f5dbd0b78ba28c3
Tincho228 <martin_quintero521@hotmail.com>
2019-10-25 17:15:18 -0300

    Responsive chages

--- Lesson6-Preston/js/windchill.js
+++ Lesson6-Preston/js/windchill.js
@@ -1,6 +1,8 @@
    
-var t = parseFloat(document.getElementById("temperature").innerHTML);    var s = parseFloat(document.getElementById("speed").innerHTML);
-if (t <= 50 && s <= 4.8 ){ 
+var t = parseFloat(document.getElementById("temperature").innerHTML);  
+var s = parseFloat(document.getElementById("speed").innerHTML);
+var f = 0;
+if (t <= 50 && s > 3 ){ 
         f= 35.74 + (0.6215 * t) - (35.75 * Math.pow (s, 0.16)) + (0.4275 * t * Math.pow (s, 0.16));
         f= Math.round(f);
         document.getElementById("windchill").innerHTML = f;

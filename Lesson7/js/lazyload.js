3a9ab9cb8ccea4dd5f5edce09375f8617ffa65ea
Tincho228 <martin_quintero521@hotmail.com>
2019-10-30 07:09:06 -0300

    Lazyload-scroll

--- Lesson7/js/lazyload.js
+++ Lesson7/js/lazyload.js
@@ -1,13 +1,15 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 100px 0px"
+};
+
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
+  image.onload = () => {image.removeAttribute('data-src');};
 };
-imagesToLoad.forEach((img) => {
-  loadImages(img);
-});
 
 if('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((items, observer) => {
